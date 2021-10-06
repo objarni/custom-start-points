65ee949fa93a2996ccbf1aac844f41c2be2fa568
emily <emily@bacheconsulting.com>
2021-10-06 16:34:52 +0200

    first draft of Tennis1 in C

---
+++ start-points/Tennis/C1/tennis_tests.c
@@ -0,0 +1,254 @@
+#include <cgreen/cgreen.h>
+#include "tennis.h"
+
+Describe(tennis);
+BeforeEach(tennis) {}
+AfterEach(tennis) {}
+
+static int maxScore(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+void setScoreTo(struct TennisGame* game, int player1Score, int player2Score) {
+    for (int i = 0; i < maxScore(player1Score, player2Score); i++)
+    {
+        if (i < player1Score)
+            TennisGame_WonPoint(game, "player1");
+        if (i < player2Score)
+            TennisGame_WonPoint(game, "player2");
+    }
+}
+
+Ensure(tennis, Love_All) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 0, 0);
+
+    assert_string_equal("Love-All", TennisGame_GetScore(game));
+    
+    free(game);
+}
+
+Ensure(tennis, Fifteen_All_1_1) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 1, 1);
+    assert_string_equal("Fifteen-All", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Thirty_All_2_2) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 2, 2);
+    assert_string_equal("Thirty-All", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Deuce_3_3) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 3, 3);
+    assert_string_equal("Deuce", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Deuce_4_4) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 4, 4);
+    assert_string_equal("Deuce", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Fifteen_Love_1_0) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 1, 0);
+    assert_string_equal("Fifteen-Love", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Love_Fifteen_0_1) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 0, 1);
+    assert_string_equal("Love-Fifteen", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Thirty_Love_2_0) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 2, 0);
+    assert_string_equal("Thirty-Love", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Love_Thirty_0_2) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 0, 2);
+    assert_string_equal("Love-Thirty", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Forty_Love_3_0) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 3, 0);
+    assert_string_equal("Forty-Love", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Love_Forty_0_3) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 0, 3);
+    assert_string_equal("Love-Forty", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player1_4_0) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 4, 0);
+    assert_string_equal("Win for player1", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player2_0_4) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 0, 4);
+    assert_string_equal("Win for player2", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Thirty_Fifteen_2_1) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 2, 1);
+    assert_string_equal("Thirty-Fifteen", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Fifteen_Thirty_1_2) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 1, 2);
+    assert_string_equal("Fifteen-Thirty", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Forty_Fifteen_3_1) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 3, 1);
+    assert_string_equal("Forty-Fifteen", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Fifteen_Forty_1_3) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 1, 3);
+    assert_string_equal("Fifteen-Forty", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player1_4_1) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 4, 1);
+    assert_string_equal("Win for player1", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player2_1_4) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 1, 4);
+    assert_string_equal("Win for player2", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Forty_Thirty_3_2) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 3, 2);
+    assert_string_equal("Forty-Thirty", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Thirty_Forty_2_3) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 2, 3);
+    assert_string_equal("Thirty-Forty", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player1_4_2) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 4, 2);
+    assert_string_equal("Win for player1", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player2_2_4) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 2, 4);
+    assert_string_equal("Win for player2", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Advantage_player1_4_3) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 4, 3);
+    assert_string_equal("Advantage player1", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Advantage_player2_3_4) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 3, 4);
+    assert_string_equal("Advantage player2", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Advantage_player1_5_4) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 5, 4);
+    assert_string_equal("Advantage player1", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Advantage_player2_4_5) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 4, 5);
+    assert_string_equal("Advantage player2", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Advantage_player1_15_14) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 15, 14);
+    assert_string_equal("Advantage player1", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Advantage_player2_14_15) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 14, 15);
+    assert_string_equal("Advantage player2", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player1_6_4) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 6, 4);
+    assert_string_equal("Win for player1", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player2_4_6) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 4, 6);
+    assert_string_equal("Win for player2", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player1_16_14) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 16, 14);
+    assert_string_equal("Win for player1", TennisGame_GetScore(game));
+    free(game);
+}
+
+Ensure(tennis, Win_for_player2_14_16) { 
+    struct TennisGame* game = TennisGame_Create("player1", "player2");
+    setScoreTo(game, 14, 16);
+    assert_string_equal("Win for player2", TennisGame_GetScore(game));
+    free(game);
+}
